3ee1c4cc218e13416fd4d56ae2b6e6990c39d543
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-08-28 17:31:34 +0530

    recipe-list clickable

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { RecipeModel } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +6,7 @@ import { RecipeModel } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected=new EventEmitter<RecipeModel>();
   recipes:RecipeModel[]=[
     new RecipeModel('A Test Recipe','This is test description','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1474468244%2Froasted-kabocha-with-maple-syrup-and-ginger-XL-RECIPE1116.jpg%3Fitok%3DS1k9seN7&w=800&q=85')
   ,new RecipeModel('A Test Recipe','This is test description','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1474468244%2Froasted-kabocha-with-maple-syrup-and-ginger-XL-RECIPE1116.jpg%3Fitok%3DS1k9seN7&w=800&q=85')
@@ -15,5 +16,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+onRecipeSelected(recipe:RecipeModel){
+this.recipeWasSelected.emit(recipe);
 
+
+}
 }
