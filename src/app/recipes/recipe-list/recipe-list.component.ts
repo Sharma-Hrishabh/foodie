1ba6591b192c184e1eefb15910b183d1d767863b
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-08-28 17:58:46 +0530

    data-binding in shopping-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ export class RecipeListComponent implements OnInit {
  @Output() recipeWasSelected=new EventEmitter<RecipeModel>();
   recipes:RecipeModel[]=[
     new RecipeModel('A Test Recipe','This is test description','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1474468244%2Froasted-kabocha-with-maple-syrup-and-ginger-XL-RECIPE1116.jpg%3Fitok%3DS1k9seN7&w=800&q=85')
-  ,new RecipeModel('A Test Recipe','This is test description','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1474468244%2Froasted-kabocha-with-maple-syrup-and-ginger-XL-RECIPE1116.jpg%3Fitok%3DS1k9seN7&w=800&q=85')
+  ,new RecipeModel('Another Test Recipe','This is test description','https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1474468244%2Froasted-kabocha-with-maple-syrup-and-ginger-XL-RECIPE1116.jpg%3Fitok%3DS1k9seN7&w=800&q=85')
 
 ];
   constructor() { }
